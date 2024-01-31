73c300c4d7d0362fc7e24ec0bc8cc8a582069abf
Farina Tahsin Chowdhury <farina@Farinas-MacBook-Air.local>
2024-01-31 15:19:31 +0600

    indicator bug fixed

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { data } from '../../data'
 import './mainpage.css'
 import '../MakeSortable/makesortable.css'
-import MakeSortable from '../MakeSortable/MakeSortable'
-import SingleComponent from '../SingleComponent/SingleComponent'
+// import MakeSortable from '../MakeSortable/MakeSortable'
+// import SingleComponent from '../SingleComponent/SingleComponent'
 import MakeSortableNew from '../MakeSortableNew/MakeSortableNew'
 
 const MainPage = () => {
@@ -20,28 +20,30 @@ const MainPage = () => {
   return (
     <div className='mainpage'>
         Sort the items!
+        <div style={{maxWidth:"600px"}}>
         <MakeSortableNew dataComponents={dataComponents}
                         setDataComponents={setDataComponents}>
           {
             dataComponents.map((item,index) => {
               return (
-                  // <div key={item.id}>
-                    <div key={item.id} className='single-div-class' 
+                  <div key={item.id}>
+                    <div key={item.id} id={`div${index}`} className='single-div-class' 
                         style={{backgroundColor:item.color}}
                         draggable={isDragging ? 'true' : 'false'}>
-                    <hr className="indicator" id={`upper${index}`}/>
-                    <div id={`dragbox${item.id}`}className='drag-div' 
+                    <hr className="indicator upper" id={`upper${index}`}/>
+                    <div className='drag-div' 
                          onMouseDown={()=>setIsDragging(true)}
                          onMouseUp={()=>setIsDragging(false)}>
                     </div>
-                    <hr className="indicator" id={`lower${index}`}/>
+                    <hr className="indicator lower" id={`lower${index}`}/>
                     {item.text}
                     </div>
-                  // </div>
+                   </div>
                 )
             })
           }
         </MakeSortableNew>
+        </div>
     </div>
   )
 }
--- src/components/MakeSortableNew/MakeSortableNew.jsx
+++ src/components/MakeSortableNew/MakeSortableNew.jsx
@@ -9,7 +9,8 @@ const MakeSortableNew = ({
 }) => {
   const [draggedIndex, setDraggedIndex] = useState(null);
   const [targetIndex, setTargetIndex] = useState(null);
-  const [prevIndicator, setPrevIndicator] = useState(null)
+  const [toUpperHalf, setToUpperHalf] = useState(null);
+  const [toLowerHalf, setToLowerHalf] = useState(null)
 
   const handleDragStart = (e, id, index) => {
     setDraggedIndex(index);
@@ -19,12 +20,17 @@ const MakeSortableNew = ({
     e.preventDefault();
 
     setTargetIndex(index);
-
+    const targetElement = e.currentTarget
+    // console.log(index)
     const mouseY = e.clientY;
+    const mouseX = e.clientX;
+    
     const componentRect = e.target.getBoundingClientRect();
     const componentHeight = componentRect.height;
+    const componentLeft = componentRect.left;
+    const componentRight = componentRect.right;
     const componentTop = componentRect.top;
-    const componentBottom = componentTop + componentHeight;
+    const componentBottom = componentRect.bottom //componentTop + componentHeight;
 
     const distanceFromTop = mouseY - componentTop;
     const distanceFromBottom = componentBottom - mouseY;
@@ -32,34 +38,54 @@ const MakeSortableNew = ({
     const percentageFromTop = (distanceFromTop / componentHeight) * 100;
     const percentageFromBottom = (distanceFromBottom / componentHeight) * 100;
 
-    if(draggedIndex !== index) {
-      if(percentageFromTop < 50) {
-      document.getElementById(`upper${index}`).style.setProperty("visibility", "visible")
+    let indicatorArray = [...document.getElementsByClassName("indicator")]
+    indicatorArray.forEach(item => {
+      item.style.setProperty("visibility", "hidden")
+    })
+    let divArray = [...document.getElementsByClassName("single-div-class")]
+      divArray.forEach(item => {
+      item.style.opacity = "1"
+    })
+    document.getElementById(`div${index}`).style.opacity = "40%"
+    if(index >= 0 && index <= dataComponents.length-1 ) {
+      let indicatorArray = [...document.getElementsByClassName("indicator")]
+      indicatorArray.forEach(item => {
+      item.style.setProperty("visibility", "hidden")
+    })
+    }
 
-      setPrevIndicator(prevItem => {
-        prevItem && document.getElementById(prevItem).style.setProperty("visibility", "hidden")
-        return `upper${index}`
-      })
-     }
+    if(draggedIndex !== index && (mouseX >= componentLeft-50 && mouseX <= componentRight+50)) {
+      if(percentageFromTop < 50) {
+        document.getElementById(`upper${index}`).style.setProperty("visibility", "visible")
+        if(draggedIndex <= index)
+          setTargetIndex(Math.max(0, index-1))
+        // else setTargetIndex(index)
+        else if(draggedIndex >= index)
+          setTargetIndex(Math.max(0, index))
+        // else setTargetIndex(index)                    //target index 0 => visibility hidden if mouseY < componentTop-20
+        // setToUpperHalf(index)                         ////target index length-1 => visibility hidden if mouseY > componentBottom+20
+      }
       else if(percentageFromBottom < 50) {
-      document.getElementById(`lower${index}`).style.setProperty("visibility", "visible") 
-      setPrevIndicator(prevItem => {
-        prevItem && document.getElementById(prevItem).style.setProperty("visibility", "hidden")
-        return `lower${index}`
-      })
+        document.getElementById(`lower${index}`).style.setProperty("visibility", "visible")
+        if(draggedIndex >= index) {
+          setTargetIndex(Math.min(dataComponents.length-1, index+1))
+        }
+        // else setTargetIndex(index)
+        else if(draggedIndex <= index) {
+          setTargetIndex(Math.min(dataComponents.length-1, index))
+        }
+        // else setTargetIndex(index)
+        // setToLowerHalf(Math.max(dataComponents.length, index+1))  
+      }
+      else {
+        document.getElementById(`lower${index}`).style.setProperty("visibility", "hidden") 
       }
     }
-    // let hrArray = [...document.getElementsByTagName("hr")]
-    // hrArray.forEach((item,index) => {           //calculate
-    //   if(index !== targetIndex)
-    //     item.style.setProperty("visibility", "hidden")
-    // })
   };
 
   const handleDrop = (e, id) => {
     e.preventDefault();
-    console.log("drop")
-    // 
+
     if (draggedIndex !== null && targetIndex !== null) {
       const updatedComponents = [...dataComponents];
       const [draggedComponent] = updatedComponents.splice(draggedIndex, 1);
@@ -68,39 +94,44 @@ const MakeSortableNew = ({
       setDataComponents(updatedComponents);
       setDraggedIndex(null);
       setTargetIndex(null);
-      
     }
     else {
       setDataComponents(setDataComponents)
       setDraggedIndex(null)
       setTargetIndex(null)
     }
+    let divArray = [...document.getElementsByClassName("single-div-class")]
+      divArray.forEach(item => {
+      item.style.opacity = "1"
+    })
+    let indicatorArray = [...document.getElementsByClassName("indicator")]
+    indicatorArray.forEach(item => {
+      item.style.setProperty("visibility", "hidden")
+    })
+  };
+  function handleDragEnd() {
+    setDraggedIndex(null);
+    setTargetIndex(null);
+    let divArray = [...document.getElementsByClassName("single-div-class")]
+      divArray.forEach(item => {
+      item.style.opacity = "1"
+    })
     let hrArray = [...document.getElementsByClassName("indicator")]
     hrArray.forEach(item => {
       item.style.setProperty("visibility", "hidden")
     })
-  };
-
-  // const handleDragEnd = () => {
-  //   console.log("end")
-  //   document.getElementsByClassName("indicator").style.setProperty("visibility", "hidden")
-  //   setDraggedIndex(null);
-  //   setTargetIndex(null);
-  //   setIsDragging(false);
-    
-  // }
-//  console.log(dataComponents)
+  }
  
   return (
     <>
       {children.map((item, index) => (
         <div
           key={index}
+          // style={{backgroundColor:'red'}}
           onDragStart={(e) => handleDragStart(e, item.key, index)}
-          // onDragEnd={handleDragEnd}
           onDragOver={(e) => handleDragOver(e, item.key, index)}
-          onDrop={(e) => handleDrop(e, item.key, index)}
-        >
+          onDragEnd={handleDragEnd}
+          onDrop={(e) => handleDrop(e, item.key, index)} >
           {item}
         </div>
       ))}  
